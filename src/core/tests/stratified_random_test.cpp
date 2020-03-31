7ba489cc6487a1393c5d8076d80aaad4d089609b
Phil26AT <phil.lindenberger@gmail.comD>
2020-03-31 10:56:42 +0200

    minor fix in stratified random test

--- src/core/tests/stratified_random_test.cpp
+++ src/core/tests/stratified_random_test.cpp
@@ -6,6 +6,7 @@ using namespace boost::ut;
 using namespace boost::ut::bdd;
 
 int main() {
+    srand(1994);
     "predict_true"_test = [] {
         given("I have the arguments N, di") = [] {
 
@@ -17,8 +18,8 @@ int main() {
                 stratified_random(N, di);
 
                 then("I get the filled array di as expected") = [=] {
-                    double exact_di[5] = {0.100000000000, 0.368037543431, 0.478876585364,
-                                            0.756619844752, 0.959688006695};
+                    double exact_di[5] = {0.117519159763, 0.230069380919,
+                             0.491273727683, 0.753994618894, 0.985284319420};
                     for (int i = 0; i < N; i++) {
                         double error = fabs(di[i]-exact_di[i]);
                         expect(that % error < 1e-12) << i;
