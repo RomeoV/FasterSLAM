253889e8d44c2da1d983ae45647c33faf2b38db9
Phil26AT <phil.lindenberger@gmail.comD>
2020-04-01 21:04:04 +0200

    Added Matrix3d to typedef

--- src/core/typedefs.h
+++ src/core/typedefs.h
@@ -3,6 +3,7 @@
 typedef double* Vector2d;
 typedef double* Vector3d;
 typedef double* Matrix2d;
+typedef double* Matrix3d;
 typedef const double* cVector2d;
 typedef const double* cVector3d;
 typedef const double* cMatrix2d;
