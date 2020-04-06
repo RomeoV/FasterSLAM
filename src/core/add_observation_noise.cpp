1a01569cfd37187bb78471cf04ac19257b8d054f
ntselepidis <ntselepidis@student.ethz.ch>
2020-04-07 00:54:55 +0200

    First modifications.

--- src/core/add_observation_noise.cpp
+++ src/core/add_observation_noise.cpp
@@ -1,87 +1,19 @@
 #include "add_observation_noise.h"
-#if 0
-//http://moby.ihme.washington.edu/bradbell/mat2cpp/randn.cpp.xml
-MatrixXd randn(int m, int n) 
-{	
-	// use formula 30.3 of Statistical Distributions (3rd ed)
-	// Merran Evans, Nicholas Hastings, and Brian Peacock
-	int urows = m * n + 1;
-	MatrixXd u(urows, 1);
 
-	//u is a random matrix
-	for (int r=0; r<urows; r++) {
-		for (int c=0; c<1; c++) {
-			u(r,c) = std::rand();
-		}
-	}
-	
-	MatrixXd x(m,n);
-
-	int i, j, k;
-	double pi = 4. * std::atan(1.f);
-	double square, amp, angle;
-	k = 0;
-	for(i = 0; i < m; i++)
-	{	for(j = 0; j < n; j++)
-		{	if( k % 2 == 0 )
-			{	square = - 2. * std::log( u(k, 0) );
-				if( square < 0. )
-					square = 0.;
-				amp = sqrt(square);
-				angle = 2. * pi * u(k+1, 0);
-				x(i, j) = amp * std::sin( angle );
-			}
-			else	
-				x(i, j) = amp * std::cos( angle );
-			k++;
-		}
-	}
-	return x;
-}
-
-MatrixXd rand(int m, int n) 
-{	
-	MatrixXd x(m,n);	
-	int i, j;
-	double rand_max = double(RAND_MAX);
-
-	for(i = 0; i < m; i++) {	
-		for(j = 0; j < n; j++)
-			x(i, j) = double(std::rand()) / rand_max;
-	} 
-	return x;
-}
-#endif
-//add random measurement noise. We assume R is diagnoal matrix
-void add_observation_noise(vector<Vector2d> &z, Matrix2d R, int addnoise)
+//! Adds random measurement noise. We assume R is diagnoal matrix.
+//! TOCHECK: vector<Vector2d> z -> Storage choice for z, ROW-WISE for now
+void add_observation_noise(double *z, const int zlen, cMatrix2d R, const int addnoise)
 {
-    double LO = -1.0f;
-    double HI = 1.0f;
+    if ( addnoise == 1 && zlen > 0 ){
+        double randM1[zlen], randM2[zlen];
+        fill_rand(randM1, zlen, -1.0, 1.0);
+        fill_rand(randM2, zlen, -1.0, 1.0);
 
-	if (addnoise == 1){
-		int len = z.size();	
-		if (len > 0) {
-			//MatrixXd randM1 = nRandMat::randn(1,len);
-            MatrixXd randM1(1,len);
-            for (int i=0; i< len; i++) {
-                double r3 = LO + (double)rand()/((double)RAND_MAX/(HI-LO));
-                randM1(0,i) = r3; 
-            }
-			//MatrixXd randM2 = nRandMat::randn(1,len);
-			MatrixXd randM2(1,len);
-            for (int j=0; j< len; j++) {
-                double r4 = LO + (double)rand()/((double)RAND_MAX/(HI-LO));
-                randM2(0,j) = r4; 
-            }
-            //cout<<"randM1"<<endl;
-            //cout<<randM1<<endl;
-
-			for (int c=0; c<len; c++) {
-				z[c][0] = z[c][0] + randM1(0,c)*sqrt(R(0,0));
-				z[c][1] = z[c][1] + randM2(0,c)*sqrt(R(1,1));
-			}
-		}
+        const double sqrtR00 = sqrt(R[0]);
+        const double sqrtR11 = sqrt(R[3]);
+        for (int i = 0; i < zlen; i++) {
+            z[i*2+0] += randM1[i]*sqrtR00; // z[i][0] = z[i][0] + randM1[i]*sqrtR00;
+            z[i*2+1] += randM2[i]*sqrtR11; // z[i][1] = z[i][1] + randM2[i]*sqrtR11;
+        }
 	}	
 }
-
-
--- src/core/add_observation_noise.h
+++ src/core/add_observation_noise.h
@@ -1,17 +1,4 @@
-#ifndef ADD_OBSERVATION_NOISE_H
-#define ADD_OBSERVATION_NOISE_H
-
-#include <Eigen/Dense>
-#include <iostream>
-#include <vector>
-
-using namespace Eigen;
-using namespace std;
-
-namespace nRandMat{
-	MatrixXd randn(int m, int n); //Gaussian distribution
-	MatrixXd rand(int m, int n); //Standard random
-}
+#pragma once
 
 /*!
     Adds random observation noise to the observation vector. [Compute intensive]
@@ -19,6 +6,4 @@ namespace nRandMat{
     @param[in]  R        	Covariance matrix of observation (diagonal).
     @param[in]  addnoise	Flag if obersvation noise should be added.
  */
-void add_observation_noise(vector<Vector2d> &z, Matrix2d R, int addnoise);
-
-#endif //ADD_OBSERVATION_NOISE_H
+void add_observation_noise(double *z, const int lenz, cMatrix2d R, const int addnoise);
