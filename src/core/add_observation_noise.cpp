d22848ca080f68d0f3e49fcd2a567be26743c2c9
ntselepidis <ntselepidis@student.ethz.ch>
2020-04-07 00:59:01 +0200

    Switched to dynamic memory allocation.

--- src/core/add_observation_noise.cpp
+++ src/core/add_observation_noise.cpp
@@ -5,7 +5,8 @@
 void add_observation_noise(double *z, const int zlen, cMatrix2d R, const int addnoise)
 {
     if ( addnoise == 1 && zlen > 0 ){
-        double randM1[zlen], randM2[zlen];
+        double *randM1 = (double*)malloc(zlen*sizeof(double));
+        double *randM2 = (double*)malloc(zlen*sizeof(double)); 
         fill_rand(randM1, zlen, -1.0, 1.0);
         fill_rand(randM2, zlen, -1.0, 1.0);
 
@@ -15,5 +16,7 @@ void add_observation_noise(double *z, const int zlen, cMatrix2d R, const int add
             z[i*2+0] += randM1[i]*sqrtR00; // z[i][0] = z[i][0] + randM1[i]*sqrtR00;
             z[i*2+1] += randM2[i]*sqrtR11; // z[i][1] = z[i][1] + randM2[i]*sqrtR11;
         }
+        free(randM1);
+        free(randM2);
 	}	
 }
