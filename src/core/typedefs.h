65efb434c9f53f0fdf1786f2d290201888354197
Romeo Valentin <rvalentin@student.ethz.ch>
2020-04-12 22:06:03 +0200

    Changed vector typedefs to static arrays

--- src/core/typedefs.h
+++ src/core/typedefs.h
@@ -1,5 +1,6 @@
 #pragma once
 
+/*
 typedef double* Vector2d;
 typedef double* Vector3d;
 typedef double* Matrix2d;
@@ -7,3 +8,11 @@ typedef double* Matrix3d;
 typedef const double* cVector2d;
 typedef const double* cVector3d;
 typedef const double* cMatrix2d;
+*/
+typedef double Vector2d[2];
+typedef double Vector3d[3];
+typedef double Matrix2d[4];
+typedef double Matrix3d[9];
+typedef const double cVector2d[2];
+typedef const double cVector3d[3];
+typedef const double cMatrix2d[4];
