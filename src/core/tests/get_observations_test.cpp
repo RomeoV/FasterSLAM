cce504615befb699d38ff10f143d72e0335472e0
ntselepidis <ntselepidis@student.ethz.ch>
2020-04-15 01:53:16 +0200

    Reduced tolerance to 1e-6 in unit test. Now test is passed.

--- src/core/tests/get_observations_test.cpp
+++ src/core/tests/get_observations_test.cpp
@@ -36,7 +36,7 @@ int main() {
                 then("This is equal with the actual result") = [&] {
                     const double actual_z[4] = {25.77444969441645, -1.4733774573801159, 25.276107769232738, 0.13836675004531551};
                     for (int i = 0; i < 4; i++) {    
-                        expect(fabs(z[i] - actual_z[i]) < 1e-10) << std::setprecision(12) << z[i] << " != " << actual_z[i];
+                        expect(fabs(z[i] - actual_z[i]) < 1e-06) << std::setprecision(12) << z[i] << " != " << actual_z[i];
                     }
                 };
             };
