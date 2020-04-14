50ca9d29840c186e888382ee7ce9b0c542f5ddd4
ntselepidis <ntselepidis@student.ethz.ch>
2020-04-14 20:35:11 +0200

    Work on get_observations_test.cpp (Not completed yet -> Segfault).

--- src/core/tests/get_observations_test.cpp
+++ src/core/tests/get_observations_test.cpp
@@ -1,6 +1,6 @@
 #include "get_observations.h"  // import file to test
-#include <cmath>
-#include <cstdio>
+#include <math.h>
+#include <stdio.h>
 
 #include "ut.hpp"
 using namespace boost::ut;
@@ -10,33 +10,38 @@ int main() {
 
     "get_observations"_test = [] {
         given("I have some arguments") = [] {
-            const double rmax = 30;
+            
+            const double rmax = 30; 
             const double x[3] = {0.674090417131751, -0.030904471130924017, -0.0073589032333721818};
-
-            size_t lm_cols = 35;
-            double *lm = (double*) malloc( 2*lm_cols * sizeof(double) ); // to fill from lm.txt
-
+            double z[4] = {};
+            
             size_t nidf = 35;
             int *idf = (int*) malloc( nidf * sizeof(int) );
             for (int i = 0; i < nidf; i++) {
                 idf[i] = i;
             }
 
-            double z[4];
-
-            when("I call get_observations()") = [&] {
-
-                // get_observations(x, lm, lm_cols, &idf, &nidf, rmax, z);
-
-                then("This is equal with the actual result") = [&] {
-                    const double actual_z[4] = {25.77444969441645, -1.4733774573801159, 25.276107769232738, 0.13836675004531551};
-                    for (int i = 0; i < 4; i++) {    
-                        expect(fabs(z[i] - actual_z[i]) < 1e-10);
-                    }
-                };
-            }; 
-            free(lm);
-            free(idf);
+//            FILE* fp = fopen("lm.txt", "r"); 
+//            size_t lm_cols = 35;
+//            double *lm = (double*) malloc( 2*lm_cols * sizeof(double) ); 
+//            for (size_t i = 0; i < lm_cols; i++){
+//                fscanf(fp, "%lf\t%lf\n", &lm[0*lm_cols+i], &lm[1*lm_cols+i]);
+//            }
+//
+//            when("I call get_observations()") = [&] {
+//
+//                // get_observations(x, lm, lm_cols, &idf, &nidf, rmax, z);
+//
+//                then("This is equal with the actual result") = [&] {
+//                    const double actual_z[4] = {25.77444969441645, -1.4733774573801159, 25.276107769232738, 0.13836675004531551};
+//                    for (int i = 0; i < 4; i++) {    
+//                        expect(fabs(z[i] - actual_z[i]) < 1e-10);
+//                    }
+//                };
+//            }; 
+//            fclose(fp);
+//            free( lm );
+            free( idf );
         };
     };
 
