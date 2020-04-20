e56dd045c6c2c0e42160cdb37a486d8fb21d7d89
Romeo Valentin <rvalentin@student.ethz.ch>
2020-04-20 19:07:43 +0200

    Fixed pi_to_pi from [-PI,PI] to (-PI,PI)

--- src/core/tests/pi_to_pi_test.cpp
+++ src/core/tests/pi_to_pi_test.cpp
@@ -16,14 +16,13 @@ int main() {
                 
                 double computed_angle = pi_to_pi(angle);
 
-                then("I get the clipped angle in the range [-PI, PI]") = [=] {
+                then("I get the clipped angle in the range (-PI, PI]") = [=] {
 
                     double exact_angle = M_PI;
                     double error_same = fabs(computed_angle - exact_angle);
-                    double error_2pi = fabs(error_same - 2*M_PI);
                     
-                    expect(that % -M_PI <= computed_angle and computed_angle <= M_PI);
-                    expect(that % error_same < 1e-14 or error_2pi < 1e-14);
+                    expect(that % -M_PI < computed_angle and computed_angle <= M_PI);
+                    expect(that % error_same < 1e-14);
                 };
             };
         };
@@ -38,15 +37,38 @@ int main() {
                 
                 pi_to_pi_arr(angles,N);
 
-                then("I get the clipped angles in the range [-PI, PI]") = [=] {
+                then("I get the clipped angles in the range (-PI, PI]") = [=] {
 
-                    double exact_angles[N] = {M_PI,0.0,-M_PI,-M_PI/2,M_PI};
+                    double exact_angles[N] = {M_PI,0.0,+M_PI,-M_PI/2,M_PI};
                     for (int i = 0; i<N; i++){
                         double error_same = fabs(angles[i] - exact_angles[i]);
-                        double error_2pi = fabs(error_same - 2*M_PI);
                         
-                        expect(that % -M_PI <= angles[i] and angles[i] <= M_PI) << i;
-                        expect(that % error_same < 1e-14 or error_2pi < 1e-14) << i;
+                        expect(that % -M_PI < angles[i] and angles[i] <= M_PI) << i;
+                        expect(that % error_same < 1e-14) << i;
+                    }
+                };
+            };
+        };
+    };
+
+    "pi_to_pi_fmod"_test = [] {
+        given("I have the argument angles, N") = [] {
+            const size_t N=5;
+            double angles[N] = {-5*M_PI, -2*M_PI, -M_PI, 1.5*M_PI, 5*M_PI};
+
+            when("I call pi_to_pi_fmod(angle)") = [&] {
+                for (size_t i = 0; i < N; i++) {
+                    angles[i] = pi_to_pi_fmod(angles[i]);
+                }
+
+                then("I get the clipped angles in the range (-PI, PI]") = [=] {
+
+                    double exact_angles[N] = {M_PI,0.0,+M_PI,-M_PI/2,M_PI};
+                    for (int i = 0; i<N; i++){
+                        double error_same = fabs(angles[i] - exact_angles[i]);
+                        
+                        expect(that % -M_PI < angles[i] and angles[i] <= M_PI) << i;
+                        expect(that % error_same < 1e-14) << i;
                     }
                 };
             };
