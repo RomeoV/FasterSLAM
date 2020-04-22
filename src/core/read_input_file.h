be1a337d56fd77dd80898bc5a18ea57e5f0563f2
Romeo Valentin <rvalentin@student.ethz.ch>
2020-04-22 16:58:52 +0200

    Added cpp-style parser (mostly copied from yglee)
    Untested!

---
+++ src/core/read_input_file.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <algorithm>
+#include <iterator>
+#include <unistd.h>
+#include <errno.h>
+#include <string>
+#include <vector>
+
+void read_input_file(const std::string s, double *lm, double *wp); 
