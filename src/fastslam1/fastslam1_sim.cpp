8799620ecc47bfa9f5fb2e4351d3b6d16e794118
ntselepidis <ntselepidis@student.ethz.ch>
2020-04-22 21:41:09 +0200

    Completed fastslam setup in main loop.

--- src/fastslam1/fastslam1_sim.cpp
+++ src/fastslam1/fastslam1_sim.cpp
@@ -15,86 +15,68 @@
 
 int counter = 0;
 
+typedef struct {
+    double xtrue[3];
+    double veh[2][3];
+    double alpha; // initial steer angle, used to be called G in yglee
+} Vehicle;
+
+// allocs and inits array of particles (its size will change)
+// allocs and inits particle weights as uniform
+void setup_initial_particles(Particle **p_, double **w_);
+void setup_initial_Q_R();
+void setup_initial_vehicle(Vehicle* v);
+void setup_landmarks(size_t **ftag_, double **da_table_, const size_t N_features);
+void setup_measurements(double **z, size_t **ftag_visible);
+void destroy_particles(Particle **p_, double **w_);
+void destroy_landmarks(size_t **ftag_, double **da_table_);
+void destroy_measurements(double **z, size_t **ftag_visible);
+
 void fastslam1_sim( double* lm, const size_t lm_rows, const size_t lm_cols, 
                     double* wp, const size_t wp_rows, const size_t wp_cols, 
                     Particle *particle ) 
 {
-    if (SWITCH_PREDICT_NOISE) {
+    const size_t N_features = lm_rows;
+
+    Particle *particles;
+    double *weights;
+    setup_initial_particles(&particles, &weights);
+
+    setup_inital_Q_R();  // modifies global variables
+
+    Vehicle vehicle_gt;
+    setup_initial_vehicle(&vehicle_gt);
+
+    size_t *ftag;
+    double *da_table;
+    setup_landmarks(&ftag, &da_table);
+
+    double *z; // range and bearings of visible landmarks ( vector<Vector2d> )
+    size_t *ftag_visible;
+    setup_measurements(&z, &ftag_visible);
+
+    if ( SWITCH_PREDICT_NOISE ) {
         printf("Sampling from predict noise usually OFF for FastSLAM 2.0\n");	
     }
+ 
+    if ( SWITCH_SEED_RANDOM ) {
+        srand( SWITCH_SEED_RANDOM );
+    }	
 
-    // normally initialized configfile.h
-    // Matrix2d declared in configfile.h
-    Q[0] = pow(sigmaV,2);
-    Q[1] = 0;
-    Q[2] = 0;
-    Q[3] = pow(sigmaG,2); 
-
-    // Matrix2d declared in configfile.h
-    R[0] = sigmaR*sigmaR;
-    R[1] = 0;
-    R[2] = 0; 
-    R[3] = sigmaB*sigmaB;
-
-    double veh[2][3] = { {0,-WHEELBASE,-WHEELBASE}, {0,-1,1} };
-
-    // vector of particles (their count will change)
-    Particle *particles = (Particle *) malloc( NPARTICLES * sizeof(Particle) );
-    for (size_t i = 0; i < NPARTICLES; i++) {
-        initParticle(&particle[i], lm_cols); // Nf = lm_cols; is this true ?
-    }
-
-    // initialize particle weights as uniform
-    double uniformw = 1.0 / (double) NPARTICLES; 
-    double* w = (double *) malloc( NPARTICLES * sizeof(double) );
-    for (size_t i = 0; i < NPARTICLES; i++) {
-        w[i] = uniformw;
-        particles[i].w = &w[i];
-    }
-
-    double xtrue[3] = {0.0, 0.0, 0.0};
-
-    double dt = DT_CONTROLS; // change in time btw predicts
-    double dtsum = 0; // change in time since last observation
-
-    size_t *ftag = (size_t*) malloc( lm_cols * sizeof(size_t) ); // identifier for each lm
-    for (size_t i = 0; i < lm_cols; i++) {
-        ftag[i] = i; 
-    }
-
-    // data association table
-    double *da_table = (double *) malloc( lm_cols * sizeof(double) );
-    for (size_t i = 0; i < lm_cols; i++) {
-        da_table[i] = -1;
-    }
-
-    int iwp = 0; // index to first waypoint
-    double G = 0; // initial steer angle
-    // MatrixXd plines; // will later change to list of points
-
-    if (SWITCH_SEED_RANDOM !=0) {
-        srand(SWITCH_SEED_RANDOM);
-    } 		
-
-    Matrix2d Qe;
-    Matrix2d Re;
-    copy(Q, 4, Qe);
-    copy(R, 4, Re);
-
-    if (SWITCH_INFLATE_NOISE ==1) {
-        scal(Qe, 4, 2.0, Qe);
-        scal(Re, 4, 2.0, Re);
-    }
-
-    int* ftag_visible;
-    double *z; // vector<Vector2d> -> range and bearings of visible landmarks
+    double dt        = DT_CONTROLS; // change in time btw predicts
+    double dtsum     = 0;           // change in time since last observation
+    int iwp          = 0;           // index to first waypoint
 
     // Main loop
     while ( iwp != -1 ) {
+//////////////////////////////////////////////////////////////////////////
+// ground_truth_update()
+//////////////////////////////////////////////////////////////////////////
+        
         compute_steering(xtrue, wp, N_wp, AT_WAYPOINT, RATEG, MAXG, dt, &iwp, &G);
-        if (iwp == -1 && NUMBER_LOOPS > 1) {
+        if ( iwp == -1 && NUMBER_LOOPS > 1 ) {
             iwp = 0;
-            NUMBER_LOOPS = NUMBER_LOOPS - 1;
+            NUMBER_LOOPS--;
         }
         predict_true(V, G, WHEELBASE, dt, xv);
 
@@ -107,20 +89,16 @@ void fastslam1_sim( double* lm, const size_t lm_rows, const size_t lm_cols,
         // Predict step	
         for (unsigned int i = 0; i < NPARTICLES; i++) {
             predict(particles[i], Vn, Gn, Qe, WHEELBASE, dt, SWITCH_PREDICT_NOISE);
-            /* if (SWITCH_HEADING_KNOWN) { */
-            /* for (int j=0; j< particles[i].xf().size(); j++) { */
-            /* Vector2d xf_j = particles[i].xf()[j]; */
-            /* xf_j[2] = xtrue[2]; */
-            /* particles[i].setXfi(j,xf_j); */	
-            /* } */           
-            /* } */
         }
 
+//////////////////////////////////////////////////////////////////////////
         //Observe step
         dtsum = dtsum+dt;
         if (dtsum >= DT_OBSERVE) {
             dtsum=0;
-
+///////////////////////////////////////////////////////////////////////////////////
+// observe()
+//////////////////////////////////////////////////////////////////////////
             //Compute true data, then add noise
             ftag_visible = vector<int>(ftag); //modify the copy, not the ftag	
 
@@ -128,9 +106,9 @@ void fastslam1_sim( double* lm, const size_t lm_rows, const size_t lm_cols,
             z = get_observations(xtrue,lm,ftag_visible,MAX_RANGE);
             add_observation_noise(z,R,SWITCH_SENSOR_NOISE);
 
-            if (!z.empty()){
-                plines = make_laser_lines(z,xtrue);
-            }
+//            if (!z.empty()){
+//                plines = make_laser_lines(z,xtrue);
+//            }
 
             //Compute (known) data associations
             int Nf = particles[0].xf().size();
@@ -159,38 +137,86 @@ void fastslam1_sim( double* lm, const size_t lm_rows, const size_t lm_cols,
             if (VnGn) { 
                 delete[] VnGn;
             }
+///////////////////////////////////////////////////////////////////////////////////
         }
     }
-    cout<<"done with all functions and will return particles"<<endl<<flush;
+    cout << "done with all functions and will return particles"<<endl<<flush;
     return particles;
 }
 
-//rb is measurements
-//xv is robot pose
-/*MatrixXd make_laser_lines(vector<Vector2d> rb, Vector3d xv) 
-{
-    if (rb.empty()) {
-        return MatrixXd(0,0);
+void setup_initial_particles(Particle **p_, double **w_) {
+    *p_ = (Particle *) malloc( NPARTICLES * sizeof(Particle) );
+    Particle *p = *p_;
+    for (size_t i = 0; i < NPARTICLES; i++) {
+        initParticle(&p[i], lm_rows);
     }
-
-    int len = rb.size();
-    MatrixXd lnes(4,len);
-
-    MatrixXd globalMat(2,rb.size());
-    int j;
-    for (j=0; j<globalMat.cols();j++) {
-        globalMat(0,j) = rb[j][0]*cos(rb[j][1]); 
-        globalMat(1,j) = rb[j][0]*sin(rb[j][1]);   	
+    const double uniform_w = 1.0 / NPARTICLES; 
+    *w_ = (double *) malloc( NPARTICLES * sizeof(double) );
+    double *w = *w_;
+    for (size_t i = 0; i < NPARTICLES; i++) {
+        w[i] = uniform_w;
+        p[i].w = &w[i];
     }
+}
 
-    TransformToGlobal(globalMat,xv);
-
-    for (int c=0; c<lnes.cols();c++) {
-        lnes(0,c) = xv(0);
-        lnes(1,c) = xv(1);
-        lnes(2,c) = globalMat(0,c);
-        lnes(3,c) = globalMat(1,c);
+void destroy_particles(Particle** p_, double** w_) {
+    Particle* p = *p_;
+    for (size_t i = 0; i < NPARTICLES; i++) {
+        delParticleMembers(p[i]);
     }
+    free(p_);
+    free(w_);
+}
 
-    return line_plot_conversion(lnes);
-}*/
+void setup_initial_Q_R() {
+
+    // Matrix2d Q, R are declared in configfile.h
+    Q[0] = pow(sigmaV, 2);
+    Q[1] = 0;
+    Q[2] = 0;
+    Q[3] = pow(sigmaG, 2); 
+    
+    R[0] = sigmaR * sigmaR;
+    R[1] = 0;
+    R[2] = 0;
+    R[3] = sigmaB * sigmaB;
+
+    if ( SWITCH_INFLATE_NOISE == 1 ) {
+        scal(Q, 4, 2.0, Q);
+        scal(R, 4, 2.0, R);
+    }
+}
+
+void setup_initial_vehicle(Vehicle* v) {
+    Vector3d xtrue_initial = {0.0, 0.0, 0.0};
+    copy(v.xtrue, 3, xtrue_initial);
+
+    double veh_initial[2][3] = { {0,-WHEELBASE,-WHEELBASE}, {0,-1,1} };
+    copy(v.veh, 2*3, veh_initial);
+
+    v.alpha = 0.0;  // along global coordinate x axis
+}
+
+void setup_landmarks(size_t **ftag_, double **da_table_, const size_t N_features) {
+    *ftag = (size_t*) malloc( N_features * sizeof(size_t) ); // identifier for each lm
+    *da_table = (double *) malloc( N_features * sizeof(double) ); // data association table
+    for (size_t i = 0; i < N_features; i++) {
+        (*ftag)[i] = i; 
+        (*da_table)[i] = -1;
+    }
+}
+
+void destroy_landmarks(size_t **ftag_, double **da_table_) {
+    free(*ftag_);
+    free(*da_table);
+}
+
+void setup_measurements(double **z, size_t **ftag_visible) {
+    *z = (double*) malloc( 2 * N_features * sizeof(double) );
+    *ftag_visible = (size_t*) malloc( N_features * sizeof(double) );
+}
+
+void destroy_measurements(double **z, size_t **ftag_visible) {
+    free(*z);
+    free(*ftag_visible);
+}
