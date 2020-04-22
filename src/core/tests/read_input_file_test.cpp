7333af7fe3e57c73ac4416e2e0864cca675a76a7
Romeo Valentin <rvalentin@student.ethz.ch>
2020-04-22 18:52:24 +0200

    Fixed memory leak

--- src/core/tests/read_input_file_test.cpp
+++ src/core/tests/read_input_file_test.cpp
@@ -12,9 +12,6 @@ int main() {
         when("I open and parse an input file with specific features") = [] {
             path input_file_path = "inputfiles_test/test_webmap.mat";
 
-            FILE* fp = fopen("inputfiles_test/test_webmap.mat", "r"); 
-            expect(fp != 0);
-
             double *lm, *wp;
             size_t N_lm, N_wp;
             // read_input_file(input_file_path.string(), lm, wp, N_lm, N_wp);
@@ -38,6 +35,8 @@ int main() {
                     expect(is_close(wp[i], i*10)) << wp[i] << " vs " << i;
                 } | std::vector<size_t>{0,1,2,3,4,5};
             };
+            free(lm);
+            free(wp);
         };
     };
 }
