e856089a9e1e94e191752fea3c5cd763c5461083
Romeo Valentin <rvalentin@student.ethz.ch>
2020-04-22 18:27:25 +0200

    Fix read_input_file and add test

--- src/core/read_input_file.h
+++ src/core/read_input_file.h
@@ -6,4 +6,7 @@
 #include <string>
 #include <vector>
 
-void read_input_file(const std::string s, double *lm, double *wp); 
+/** Reads the input file with the given path
+ * Careful: This mallocs for lm and wp and expects you to dealloc!
+ */
+void read_input_file(const std::string s, double **lm, double **wp, size_t& N_lm, size_t& N_wp); 
