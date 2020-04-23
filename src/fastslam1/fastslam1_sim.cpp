1db7d8b7ac5f4cfd23ce64e066292aefb422df38
Romeo Valentin <rvalentin@student.ethz.ch>
2020-04-23 22:05:56 +0200

    Removed refactoring artifacts

--- src/fastslam1/fastslam1_sim.cpp
+++ src/fastslam1/fastslam1_sim.cpp
@@ -34,12 +34,11 @@ void fastslam1_sim( double* lm, const size_t lm_rows, const size_t lm_cols,
     double *da_table;
     setup_landmarks(&ftag, &da_table, N_features);
 
-    double *z_, *zf_, *zn_; // range and bearings of visible landmarks ( vector<Vector2d> )
     Vector2d (*z);  // This is a dynamic array of Vector2d - see https://stackoverflow.com/a/13597383/5616591
     Vector2d (*zf);
     Vector2d (*zn);
     int *idf, *ftag_visible;
-    setup_measurements(&(z), &(zf), &(zn), &idf, &ftag_visible, N_features);
+    setup_measurements(&z, &zf, &zn, &idf, &ftag_visible, N_features);
 
 //    if ( SWITCH_PREDICT_NOISE ) {
 //        printf("Sampling from predict noise usually OFF for FastSLAM 2.0\n");	
