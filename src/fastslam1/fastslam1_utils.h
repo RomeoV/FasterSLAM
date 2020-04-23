4c7c3ba20922a5504d4f27169a77828127f2239b
Romeo Valentin <rvalentin@student.ethz.ch>
2020-04-23 22:02:38 +0200

    Changed order of copies (copied from target to src)

--- src/fastslam1/fastslam1_utils.h
+++ src/fastslam1/fastslam1_utils.h
@@ -1,12 +1,6 @@
 #pragma once
 #include "particle.h"
-
-typedef struct {
-    double xtrue[3];
-    double veh[2][3];
-    double V;
-    double alpha; // initial steer angle, used to be called G in yglee
-} Vehicle;
+#include "vehicle.h"
 
 // Setup functions
 void setup_initial_Q_R();
