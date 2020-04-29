79506a038a191a8f8fc623e2a8f97f839f67daea
Romeo Valentin <rvalentin@student.ethz.ch>
2020-04-29 22:32:52 +0200

    Removed old test (comment) and dealloc'd particle

--- src/core/tests/predict_test.cpp
+++ src/core/tests/predict_test.cpp
@@ -120,54 +120,7 @@ int main() {
                 };
             };
 
-/*
-            when("I give control inputs for a rectangle") = [=]() mutable {
-                double Q[4] = {0, 0, 0, 0};
-
-                const double V = random()*1.0/RAND_MAX;
-                const double dt = random()*1.0/RAND_MAX;
-                double intermediate_pos[3] = {1-2*V*dt, 1 - V*dt, -M_PI/2};
-
-                //predict(&p, 2*V, M_PI/2 + 0*M_PI, Q, dt);
-                //predict(&p, 1*V, M_PI/2 + 2*M_PI, Q, dt);  // +2PI to check invariance to values outside of (-PI,PI]
-                then("After half way, I'm in a different place") = [&] {
-                    "position"_test = [&](size_t i) {
-                        expect(that % fabs(intermediate_pos[i] - p.xv[i]) < 1e-14) << p.xv[i] << "!= " << intermediate_pos[i];
-                    } | std::vector{0,1,2};
-                };
-
-                //predict(&p, 2*V, M_PI/2 + 4*M_PI, Q, dt);
-                //predict(&p, 1*V, M_PI/2 - 2*M_PI, Q, dt);
-                then("In the end I end up in the same place as before") = [&] {
-                    "position"_test = [&](size_t i) {
-                        expect(that % fabs(initial_pos[i] - p.xv[i]) < 1e-14) << p.xv[i];
-                    } | std::vector{0,1,2};
-                };
-            };
-
-            when("I give control inputs for a pentagon") = [=]() mutable {
-                double Q[4] = {0, 0, 0, 0};
-
-                const double V = random()*1.0/RAND_MAX;
-                const double dt = random()*1.0/RAND_MAX;
-                //predict(&p, 1*V, M_PI*2./5 + 0*M_PI, Q, dt);
-                //predict(&p, 1*V, M_PI*2./5 + 6*M_PI, Q, dt);
-                //predict(&p, 1*V, M_PI*2./5 + 2*M_PI, Q, dt);
-                then("After 3/5 of the way, I'm in a different place") = [&] {
-                    "position"_test = [&](size_t i) {
-                        expect(that % fabs(initial_pos[i] - p.xv[i]) > 1e-14) << p.xv[i];
-                    } | std::vector{0,1,2};
-                };
-
-                //predict(&p, 1*V, M_PI*2./5 - 8*M_PI, Q, dt);
-                //predict(&p, 1*V, M_PI*2./5 - 2*M_PI, Q, dt);
-                then("I end up in the same place as before") = [&] {
-                    "position"_test = [&](size_t i) {
-                        expect(that % fabs(initial_pos[i] - p.xv[i]) < 1e-14) << p.xv[i];
-                    } | std::vector{0,1,2};
-                };
-            };
-    */
+            delParticleMembers(&p);
         };
     };
 }
