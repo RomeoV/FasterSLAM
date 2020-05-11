ef11add1532523647c363c859683779e2442c816
Jonathan Lehner <lehnerj@student.ethz.ch>
2020-05-11 23:01:36 +0200

    optim compute jacobians - added headerfile

--- src/core/compute_jacobians.h
+++ src/core/compute_jacobians.h
@@ -27,3 +27,23 @@ void compute_jacobians_base(Particle* particle,
         Matrix2d* Hf, // jacobians of function h (deriv of h wrt mean)
         Matrix2d* Sf) //measurement covariance
 ;
+
+void compute_jacobians_basic_optimisations(Particle* particle, 
+        int idf[], 
+        size_t N_z,
+        Matrix2d R, 
+        Vector2d* zp, //measurements (range, bearing)
+        Matrix23d* Hv, // jacobians of function h (deriv of h wrt pose)
+        Matrix2d* Hf, // jacobians of function h (deriv of h wrt mean)
+        Matrix2d* Sf) //measurement covariance
+;
+
+void compute_jacobians_advanced_optimisations(Particle* particle, 
+        int idf[], 
+        size_t N_z,
+        Matrix2d R, 
+        Vector2d* zp, //measurements (range, bearing)
+        Matrix23d* Hv, // jacobians of function h (deriv of h wrt pose)
+        Matrix2d* Hf, // jacobians of function h (deriv of h wrt mean)
+        Matrix2d* Sf) //measurement covariance
+;
