84beb42428c416b33754b407c0c88acde816a781
ntselepidis <ntselepidis@student.ethz.ch>
2020-05-12 01:29:24 +0200

    Duplicated KF_cholesky_update files.

---
+++ src/core/KF_cholesky_update_v2.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "typedefs.h"
+
+/*!
+    Update Kalman Filter using Cholesky decomposition. 
+    @param[out] x 	    State vector.
+    @param[out] P 	    Covariance matrix of state.
+    @param[in]  v   	Velocity at current state.
+    @param[in]  R    	Covariance matrix of measurements.
+    @param[in]  H    	Jacobian of h wrt feature states.
+*/
+void KF_cholesky_update(Vector2d x, Matrix2d P, cVector2d v, cMatrix2d R, cMatrix2d H);
+
+void KF_cholesky_update_base(Vector2d x, Matrix2d P, cVector2d v, cMatrix2d R, cMatrix2d H);
+void KF_cholesky_update_v1(Vector2d x, Matrix2d P, cVector2d v, cMatrix2d R, cMatrix2d H);
+void KF_cholesky_update_v2(Vector2d x, Matrix2d P, cVector2d v, cMatrix2d R, cMatrix2d H);
+#ifdef __AVX2__
+void KF_cholesky_update_v2_avx_v1(Vector2d x, Matrix2d P, cVector2d v, cMatrix2d R, cMatrix2d H);
+void KF_cholesky_update_v2_avx_v2(Vector2d x, Matrix2d P, cVector2d v, cMatrix2d R, cMatrix2d H);
+#endif
