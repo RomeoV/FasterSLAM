925f78cb7256e9998a1524d85ffba8b3756e2796
ntselepidis <ntselepidis@student.ethz.ch>
2020-05-16 02:14:19 +0200

    Switched to 'correct' KF_cholesky + tested compute_jacobians and feature_update against MATLAB.

---
+++ src/core/KF_cholesky_update_v0.cpp
@@ -0,0 +1,128 @@
+#include "KF_cholesky_update.h"
+#include "linalg.h"
+#include <immintrin.h>
+
+/*****************************************************************************
+ * OPTIMIZATION STATUS
+ * Done: Base implementation, unit test
+ * ToDo: Start optimizing
+ ****************************************************************************/
+
+void KF_cholesky_update(Vector2d x, Matrix2d P, cVector2d v, cMatrix2d R, cMatrix2d H) {
+    //KF_cholesky_update_base(x, P, v, R, H);
+    //KF_cholesky_update_v1(x, P, v, R, H);
+    KF_cholesky_update_v2(x, P, v, R, H);
+}
+
+/*****************************************************************************
+ * PERFORMANCE STATUS
+ * Work: 2 neg + 60 adds + 54 muls + 2 divs + 2 sqrts = 120 flops
+ * Memory moved: TBD
+ * Cycles: TBD
+ * Performance: TBD
+ * Optimal: TBD
+ * Status: TBD
+ ****************************************************************************/
+void KF_cholesky_update_base(Vector2d x, Matrix2d P, cVector2d v, cMatrix2d R, cMatrix2d H)
+{
+    double Ht[4], PHt[4], HPHt[4];
+    double S[4], St[4], SChol[4], SCholInv[4], SCholInvt[4];
+    double W1[4], W[4], Wv[2], W1t[4], W1W1t[4];
+
+    transpose(H, 2, 2, Ht);               //! Ht = H.transpose();
+    mul(P, Ht, 2, 2, 2, PHt);             //! PHt = P*Ht;
+    mul(H, PHt, 2, 2, 2, HPHt);           //! HPHt = H*PHt;
+    add(HPHt, R, 4, S);                   //! S = HPHt + R;
+
+    transpose(S, 2, 2, St);               //! St = S.transpose();
+    add(S, St, 4, S);                     //! S = S + St
+    scal(S, 4, 0.5, S);                   //! S = 0.5*S;
+
+    llt_2x2(S, SChol);                    //! S = SChol * SChol^T
+    inv_2x2(SChol, SCholInv);             //! SCholInv = inv( SChol )
+
+    mul(PHt, SCholInv, 2, 2, 2, W1);     //! W1 = PHt * SCholInv;
+    transpose(SCholInv, 2, 2, SCholInvt); //! SCholInvt = SCholInv.transpose();
+    mul(W1, SCholInvt, 2, 2, 2, W);        //! W = W1 * SCholInvt;
+
+    //! x = x + Wv;
+    mul(W, v, 2, 2, 1, Wv);
+    add(x, Wv, 2, x);
+    
+    //! P = P - W1*W1.transpose();
+    transpose(W1, 2, 2, W1t);
+    mul(W1, W1t, 2, 2, 2, W1W1t);
+    sub(P, W1W1t, 4, P);
+}
+
+void KF_cholesky_update_v1(Vector2d x, Matrix2d P, cVector2d v, cMatrix2d R, cMatrix2d H)
+{
+    double PHt[4], S[4], St[4], SChol[4], SCholInv[4];
+    double W1[4], W[4], W1W1t[4];
+
+    mmT_2x2(P, H, PHt);                 //! PHt = P*Ht
+    copy(R, 4, S);                      //! S = R;
+    mmadd_2x2(H, PHt, S);               //! S += H*PHt ( S = H*P*H^T + R )
+
+    // optimize or skip
+    transpose_2x2(S, St);               //! St = S^T
+    add(S, St, 4, S);                   //! S = S + St
+    scal(S, 4, 0.5, S);                 //! S = 0.5*S
+ 
+    llt_2x2(S, SChol);                  //! S = SChol * SChol^T
+    inv_2x2(SChol, SCholInv);           //! SCholInv = inv( SChol )
+
+    mm_2x2(PHt, SCholInv, W1);          //! W1 = PHt * SCholInv
+    mmT_2x2(W1, SCholInv, W);
+
+    mvadd_2x2(W, v, x);                 //! x = x + W*v
+
+    //! P = P - W1*W1.transpose();
+    mmT_2x2(W1, W1, W1W1t);
+    sub(P, W1W1t, 4, P);
+}
+
+void KF_cholesky_update_v2(Vector2d x, Matrix2d P, cVector2d v, cMatrix2d R, cMatrix2d H)
+{
+    double      PHt[4] __attribute__ ((aligned(32)));
+    double        S[4] __attribute__ ((aligned(32))); 
+    double       St[4] __attribute__ ((aligned(32))); 
+    double    SChol[4] __attribute__ ((aligned(32)));
+    double SCholInv[4] __attribute__ ((aligned(32)));
+    double       W1[4] __attribute__ ((aligned(32)));
+    double        W[4] __attribute__ ((aligned(32)));
+    double    W1W1t[4] __attribute__ ((aligned(32)));
+
+#ifdef __AVX2__
+    mmT_2x2_avx_v1(P, H, PHt);          //! PHt = P*Ht
+#else
+    mmT_2x2(P, H, PHt);          //! PHt = P*Ht
+#endif
+    copy(R, 4, S);                      //! S = R;
+    mmadd_2x2_avx_v1(H, PHt, S);        //! S += H*PHt ( S = H*P*H^T + R )
+
+    // optimize or skip
+    transpose_2x2(S, St);               //! St = S^T
+    add(S, St, 4, S);                   //! S = S + St
+    scal(S, 4, 0.5, S);                 //! S = 0.5*S
+ 
+    llt_2x2(S, SChol);                  //! S = SChol * SChol^T
+    inv_2x2(SChol, SCholInv);           //! SCholInv = inv( SChol )
+
+    mm_2x2_avx_v1(PHt, SCholInv, W1);   //! W1 = PHt * SCholInv
+#ifdef __AVX2__
+    mmT_2x2_avx_v1(W1, SCholInv, W);
+#else
+    mmT_2x2(W1, SCholInv, W);
+#endif
+
+    mvadd_2x2(W, v, x);                 //! x = x + W*v
+
+    //! P = P - W1*W1.transpose();
+#ifdef __AVX2__
+    mmT_2x2_avx_v1(W1, W1, W1W1t);
+#else
+    mmT_2x2(W1, W1, W1W1t);
+#endif
+    sub(P, W1W1t, 4, P);
+}
--- src/core/tests/feature_update_test.cpp
+++ src/core/tests/feature_update_test.cpp
@@ -7,7 +7,7 @@ using namespace boost::ut;
 using namespace boost::ut::bdd;
 
 int main() {
-  "update feature"_test = [] {
+/*  "update feature"_test = [] {
     given(
         "A particle with three known features and vehicle position "
       ) = [] {
@@ -55,6 +55,71 @@ int main() {
         };
        
       };
+      delParticleMembersAndFreePtr(particle);
+    };
+  };
+*/
+  "feature_update"_test = [] {
+    given("A particle with three known features and vehicle position ") = [] {
+      // prepare particle
+
+      Vector3d xv = {1.293967823315060, -0.054066219251330, -0.012642858479510};
+      
+      Particle* particle = newParticle(3, xv);
+      
+      Vector2d xf[2] = { {3.227460886446243, -25.613382543676146},
+                         {25.570128848983597, 3.630650683089399} }; // Transposed from MATLAB
+      
+      Matrix2d Pf[2] = { {0.199855490073439, 0.019180472296076, 0.019180472296076, 0.011937739684843},
+                         {0.013819565896226, -0.026186052088964, -0.026186052088964, 0.189525459865311} };
+
+      for(int i = 0; i < 2; i++){ //! 2d means of EKF in cartesian world coordinates
+        set_xfi(particle, xf[i], i);
+        set_Pfi(particle, Pf[i], i);
+      }
+
+      Vector2d z[2] = { {25.761106705273054, -1.462835729968151},
+                        {24.622003552182658, 0.206077227346340} }; // Transposed from MATLAB
+
+      size_t N_z = 2; 
+      int idf[2] = {0, 1};
+      Matrix2d R = {0.010000000000000, 0, 0, 0.000304617419787}; 
+
+      Vector2d zp[2] = { };
+      Matrix23d Hv[2] = { };
+      Matrix2d Hf[2] = { };
+      Matrix2d Sf[2] = { };
+      //compute_jacobians_base(particle, idf, 2, R, zp, Hv, Hf, Sf);
+
+      when("I update features of the particle") = [&](){
+
+        feature_update(particle, z, idf, N_z, R, zp, Hv, Hf, Sf);
+
+        auto is_close = [](double lhs, double rhs) { return fabs(lhs-rhs) < 1e-8; };
+
+        Vector2d target_xf[2] = { {3.470171202213126, -25.656742169761873},
+                                  {25.523980084381321, 4.170480246835258} };
+
+        then("I expect that the xf are equal to the arr elements.") = [=] {
+            for (int i = 0; i < 2; i++) {
+                for (int j = 0; j < 2; j++) {
+                    expect(that % is_close(particle->xf[i*2+j], target_xf[i][j]) == true);
+                }
+            }
+        };
+        
+        Matrix2d target_Pf[3] = {{0.099441292170602, 0.008341518741166, 0.008341518741166, 0.005737523050281},
+                               {0.006932154241992, -0.012983119323818, -0.012983119323818, 0.092241962970569}};
+        then("I expect that the Pf are equal to the arr elements.") = [=] {
+            for (int i = 0; i < 2; i++) {
+                for (int j = 0; j < 2; j++) {
+                    expect(that % is_close(particle->Pf[i*4+j], target_Pf[i][j]) == true);
+                }
+            }
+        };
+       
+      };
+
       delParticleMembersAndFreePtr(particle);
     };
   };
