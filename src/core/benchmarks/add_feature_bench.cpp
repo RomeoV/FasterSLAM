91bdfbb72241c7fa37d21c963900819c7c69f079
Jonathan Lehner <lehnerj@student.ethz.ch>
2020-05-16 16:38:33 +0200

    add_feature_bench

--- src/core/benchmarks/add_feature_bench.cpp
+++ src/core/benchmarks/add_feature_bench.cpp
@@ -26,11 +26,20 @@ int main() {
                 0, pow(1.0 * M_PI / 180, 2)};  // this is from the yglee config
 
     // sanity check
-    add_feature_base(&p, landmarks, 2, R);
-    add_feature(&p, landmarks, 2, R);
-
-    expect(that % fabs(VnGn[0] - VnGn_base[0]) < 1.0e-10) << "Speed+Noise";
-    expect(that % fabs(VnGn[1] - VnGn_base[1]) < 1.0e-10) << "SteeringAngle+Noise";
+    Particle p_base, p_new;
+    double xv_initial[3] =  {0,0,0};
+    Vector3d pos = {1., 1., acos(4. / 5.)};
+    initParticle(&p_base, 3, xv_initial);
+    copy(pos, 3, (&p_base)->xv);
+    (&p_base)->Nfa = 3;
+    initParticle(&p_new, 3, xv_initial);
+    copy(pos, 3, (&p_new)->xv);
+    (&p_new)->Nfa = 3;
+    add_feature_base(&p_base, landmarks, 2, R);
+    add_feature(&p_new, landmarks, 2, R);
+                
+    expect(that % fabs(p_new.xf[2 * 3 + 0] - p_base.xf[2 * 3 + 0] ) < 1e-14) << "F1 - distance";
+    expect(that % fabs(p_new.xf[2 * 3 + 1] - p_base.xf[2 * 3 + 0] ) < 1e-14) << "F1 - angle";
 
     // Initialize the benchmark struct by declaring the type of the function you want to benchmark
     Benchmark<decltype(&add_feature)> bench("add_feature Benchmark");
@@ -43,7 +52,6 @@ int main() {
     // First function should always be the base case you want to benchmark against!
     bench.add_function(&add_feature_base, "add_feature_base", work);
     bench.add_function(&add_feature, "add_feature", work);
-    //bench.add_function(&add_feature_fmod, "add_feature_fmod", work);
 
     //Run the benchmark: give the inputs of your function in the same order as they are defined. 
     bench.run_benchmark(&p, landmarks, 2, R);
