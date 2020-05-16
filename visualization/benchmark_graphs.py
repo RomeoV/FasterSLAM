c5bac62b81a7946476cc7e0708ad27fc3c9487b2
Jonathan Lehner <lehnerj@student.ethz.ch>
2020-05-17 01:54:34 +0200

    added individual plots

--- visualization/benchmark_graphs.py
+++ visualization/benchmark_graphs.py
@@ -9,10 +9,44 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 #data = pd.read_csv('../src/build/core/benchmarks/benchmark.csv')
-data = pd.read_csv('./benchmark_sample.csv')
-print(data)
+data = pd.read_csv('./benchmark_sample.csv').values
+# format: benchmark name, function name, flops, cycles, performance, speedup 
+#print(data)
+
+plot_arrays = []
+benchmark_names = []
+
+prev_array = []
+prev_key = ""
+for x in data:
+  key = x[0].split(";")[0]
+  if(prev_key == ""):
+    prev_key = key
+    prev_array.append(x[0].split(";")[1:])
+  elif(key != prev_key):
+    # print prev array
+    print(prev_key, prev_array)
+    plot_arrays.append(prev_array)
+    benchmark_names.append(prev_key)
+
+    prev_array = []
+    prev_array.append(x[0].split(";")[1:])
+    prev_key = key
+  else:
+    prev_array.append(x[0].split(";")[1:])
+print(prev_key, prev_array)
+plot_arrays.append(prev_array)
+benchmark_names.append(prev_key)
 
 sns.set(style="darkgrid")
+
+for index, benchmark in enumerate(plot_arrays):
+    titanic = pd.DataFrame(data=plot_arrays[index],
+                index=range(0, len(plot_arrays[index])),
+                columns=['function','flops','cycles','performance','speedup',''])
+    print(titanic)
+    sns.catplot(x="function", y="cycles", hue="cycles", kind="bar", data=titanic);
+    plt.show()
 
 # Load an example dataset with long-form data
 fmri = sns.load_dataset("fmri")
