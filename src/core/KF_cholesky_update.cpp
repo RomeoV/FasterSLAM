c9aed35154d55b3a26c42a09843d3415facb5574
ntselepidis <ntselepidis@student.ethz.ch>
2020-06-05 16:04:44 +0200

    Optimized further KF_cholesky_update_unrolled4_avx.

--- src/core/KF_cholesky_update.cpp
+++ src/core/KF_cholesky_update.cpp
@@ -268,21 +268,25 @@ void KF_cholesky_update_unrolled4_avx(__m256d *x0x2,
     __m256d const W2 = _mm_2x2_avx_v1(P2H2t, S2inv);
     __m256d const W3 = _mm_2x2_avx_v1(P3H3t, S3inv);
 
-    //! Permutes for stacked mvadds
-    __m256d const x2x0 = _mm256_permute2f128_pd( *x0x2, *x0x2, 0b00000001 );
-    __m256d const v2v0 = _mm256_permute2f128_pd(  v0v2,  v0v2, 0b00000001 );
-    __m256d const x3x1 = _mm256_permute2f128_pd( *x1x3, *x1x3, 0b00000001 );
-    __m256d const v3v1 = _mm256_permute2f128_pd(  v1v3,  v1v3, 0b00000001 );
+//    //! Permutes for stacked mvadds
+//    __m256d const x2x0 = _mm256_permute2f128_pd( *x0x2, *x0x2, 0b00000001 );
+//    __m256d const v2v0 = _mm256_permute2f128_pd(  v0v2,  v0v2, 0b00000001 );
+//    __m256d const x3x1 = _mm256_permute2f128_pd( *x1x3, *x1x3, 0b00000001 );
+//    __m256d const v3v1 = _mm256_permute2f128_pd(  v1v3,  v1v3, 0b00000001 );
+//
+//    //! x = x + W*v
+//    __m128d const x0 =_mvadd_2x2_avx_v1( W0, _mm256_castpd256_pd128(v0v2), _mm256_castpd256_pd128(*x0x2) );
+//    __m128d const x1 =_mvadd_2x2_avx_v1( W1, _mm256_castpd256_pd128(v1v3), _mm256_castpd256_pd128(*x1x3) );
+//    __m128d const x2 =_mvadd_2x2_avx_v1( W2, _mm256_castpd256_pd128(v2v0), _mm256_castpd256_pd128( x2x0) );
+//    __m128d const x3 =_mvadd_2x2_avx_v1( W3, _mm256_castpd256_pd128(v3v1), _mm256_castpd256_pd128( x3x1) );
+//
+//    //! Store back in x0x2, x1x3
+//    *x0x2 = _mm256_blend_pd( _mm256_broadcast_pd( &x0 ), _mm256_broadcast_pd( &x2 ), 0b1100 );
+//    *x1x3 = _mm256_blend_pd( _mm256_broadcast_pd( &x1 ), _mm256_broadcast_pd( &x3 ), 0b1100 );
 
-    //! x = x + W*v
-    __m128d const x0 =_mvadd_2x2_avx_v1( W0, _mm256_castpd256_pd128(v0v2), _mm256_castpd256_pd128(*x0x2) );
-    __m128d const x1 =_mvadd_2x2_avx_v1( W1, _mm256_castpd256_pd128(v1v3), _mm256_castpd256_pd128(*x1x3) );
-    __m128d const x2 =_mvadd_2x2_avx_v1( W2, _mm256_castpd256_pd128(v2v0), _mm256_castpd256_pd128( x2x0) );
-    __m128d const x3 =_mvadd_2x2_avx_v1( W3, _mm256_castpd256_pd128(v3v1), _mm256_castpd256_pd128( x3x1) );
-
-    //! Store back in x0x2, x1x3
-    *x0x2 = _mm256_blend_pd( _mm256_broadcast_pd( &x0 ), _mm256_broadcast_pd( &x2 ), 0b1100 );
-    *x1x3 = _mm256_blend_pd( _mm256_broadcast_pd( &x1 ), _mm256_broadcast_pd( &x3 ), 0b1100 );
+    //! x = x + W*x
+    *x0x2 = _mmTadd_2x2_avx_v2(v0v2, W0, *x0x2);
+    *x1x3 = _mmTadd_2x2_avx_v2(v1v3, W0, *x1x3);
 
     //! Z = W * (PHt)^T = P*Ht*Sinv*H*Pt
     __m256d const Z0 = _mmT_2x2_avx_v3( W0, P0H0t );
