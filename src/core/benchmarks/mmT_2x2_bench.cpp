b1ebc86cbfc77c9e1fc6d0a579c2dc51a0e003c4
ntselepidis <ntselepidis@student.ethz.ch>
2020-06-10 22:29:26 +0200

    Fixed linalg benchmarks.

--- src/core/benchmarks/mmT_2x2_bench.cpp
+++ src/core/benchmarks/mmT_2x2_bench.cpp
@@ -12,63 +12,43 @@
 using namespace boost::ut;  // provides `expect`, `""_test`, etc
 using namespace boost::ut::bdd;  // provides `given`, `when`, `then`
 
-auto data_loader(const double *A, const double *B, double *C) {
+__m256d data_loader( __m256d a, __m256d b ) {
+    return a;
+}
+
+__m256d mmT_2x2_reg( __m256d a, __m256d b ) {
+    double A[4] __attribute__ ((aligned(32)));
+    double B[4] __attribute__ ((aligned(32)));
+    double C[4] __attribute__ ((aligned(32)));
+    _mm256_store_pd(A, a);
+    _mm256_store_pd(B, b);
+    mmT_2x2(A, B, C);
+    return a; //dummy
 }
 
 int main() {
 
     // Test: 
     double A[4] __attribute__ ((aligned(32))) = {0., 1., 2., 3.};
-    double B[4] __attribute__ ((aligned(32))) = {1., 3., 2., 4.};
-    double C[4] __attribute__ ((aligned(32))) = {0., 0., 0., 0.};
-    double C1[4] __attribute__ ((aligned(32))) = {0., 0., 0., 0.};
-    double C2[4] __attribute__ ((aligned(32))) = {0., 0., 0., 0.};
-    double C3[4] __attribute__ ((aligned(32))) = {0., 0., 0., 0.};
-    double D[4] __attribute__ ((aligned(32))) = {3., 4., 11., 16.};
+    double B[4] __attribute__ ((aligned(32))) = {1., 2., 3., 4.};
+    double C[4] __attribute__ ((aligned(32))) = {3., 4., 11., 16.};
 
-    data_loader(A, B, C);
-    mmT_2x2(A, B, C);
-    
-#ifdef __AVX2__
-    data_loader(A, B, C1);
-    mmT_2x2_avx_v1(A, B, C1);
-    
-    data_loader(A, B, C2);
-    mmT_2x2_avx_v2(A, B, C2);
-    
-    data_loader(A, B, C3);
-    mmT_2x2_avx_v3(A, B, C3);
-#endif
-    
-    double error = 0.0;
-    for (int i = 0; i < 4; i++) {
-        error = fabs(  C[i] - D[i] ); expect(that % error < 1e-16) << i;
-        error = fabs( C1[i] - D[i] ); expect(that % error < 1e-16) << i + 10;
-        error = fabs( C2[i] - D[i] ); expect(that % error < 1e-16) << i + 20;
-        error = fabs( C3[i] - D[i] ); expect(that % error < 1e-16) << i + 30;
-    }
-    
-    Benchmark<decltype(&mmT_2x2)> bench("mmT_2x2 benchmark");
-    data_loader(A, B, C);
+    __m256d const a = _mm256_load_pd( A );
+    __m256d const b = _mm256_load_pd( B );
+ 
+    Benchmark<decltype(&_mmT_2x2_avx_v3)> bench("mmT_2x2 benchmark");
+
     bench.data_loader = data_loader; // To guarantee same inputs
-    // Add your functions to the struct, give it a name (Should describe improvements there) and yield the flops this function has to do (=work)
-    // First function should always be the base case you want to benchmark against!
-    bench.add_function(&mmT_2x2, "base", 0.0); 
+    
+    bench.add_function(&mmT_2x2_reg, "base", 0.0);
     bench.funcFlops[0] = mmT_2x2_flops(A, B, C);
     bench.funcBytes[0] = 8*mmT_2x2_memory(A, B, C);
-#ifdef __AVX2__
-    bench.add_function(&mmT_2x2_avx_v1, "avx_v1", 0.0);
+ 
+    bench.add_function(&_mmT_2x2_avx_v3, "avx_v3", 0.0);
     bench.funcFlops[1] = mmT_2x2_flops(A, B, C);
     bench.funcBytes[1] = 8*mmT_2x2_memory(A, B, C);
-    bench.add_function(&mmT_2x2_avx_v2, "avx_v2", 0.0);
-    bench.funcFlops[2] = mmT_2x2_flops(A, B, C);
-    bench.funcBytes[2] = 8*mmT_2x2_memory(A, B, C);
-    bench.add_function(&mmT_2x2_avx_v3, "avx_v3", 0.0);
-    bench.funcFlops[3] = mmT_2x2_flops(A, B, C);
-    bench.funcBytes[3] = 8*mmT_2x2_memory(A, B, C);
-#endif
 
-    bench.run_benchmark(A, B, C);
+    bench.run_benchmark(a, b);
 
     return 0;
 }
--- src/core/benchmarks/mm_2x2_bench.cpp
+++ src/core/benchmarks/mm_2x2_bench.cpp
@@ -12,7 +12,18 @@
 using namespace boost::ut;  // provides `expect`, `""_test`, etc
 using namespace boost::ut::bdd;  // provides `given`, `when`, `then`
 
-auto data_loader(const double *A, const double *B, double *C) {
+__m256d data_loader( __m256d a, __m256d b ) {
+    return a;
+}
+
+__m256d mm_2x2_reg( __m256d a, __m256d b ) {
+    double A[4] __attribute__ ((aligned(32)));
+    double B[4] __attribute__ ((aligned(32)));
+    double C[4] __attribute__ ((aligned(32)));
+    _mm256_store_pd(A, a);
+    _mm256_store_pd(B, b);
+    mm_2x2(A, B, C);
+    return a; //dummy
 }
 
 int main() {
@@ -20,56 +31,24 @@ int main() {
     // Test: 
     double A[4] __attribute__ ((aligned(32))) = {0., 1., 2., 3.};
     double B[4] __attribute__ ((aligned(32))) = {1., 2., 3., 4.};
-    double C[4] __attribute__ ((aligned(32))) = {0., 0., 0., 0.};
-    double C1[4] __attribute__ ((aligned(32))) = {0., 0., 0., 0.};
-    double C2[4] __attribute__ ((aligned(32))) = {0., 0., 0., 0.};
-    double C3[4] __attribute__ ((aligned(32))) = {0., 0., 0., 0.};
-    double D[4] __attribute__ ((aligned(32))) = {3., 4., 11., 16.};
+    double C[4] __attribute__ ((aligned(32))) = {3., 4., 11., 16.};
 
-    data_loader(A, B, C);
-    mm_2x2(A, B, C);
-    
-#ifdef __FMA__
-    data_loader(A, B, C1);
-    mm_2x2_avx_v1(A, B, C1);
-    
-    data_loader(A, B, C2);
-    mm_2x2_avx_v2(A, B, C2);
-    
-    data_loader(A, B, C3);
-    mm_2x2_avx_v3(A, B, C3);
-#endif
-    
-    double error = 0.0;
-    for (int i = 0; i < 4; i++) {
-        error = fabs(  C[i] - D[i] ); expect(that % error < 1e-16) << i;
-        error = fabs( C1[i] - D[i] ); expect(that % error < 1e-16) << i + 10;
-        error = fabs( C2[i] - D[i] ); expect(that % error < 1e-16) << i + 20;
-        error = fabs( C3[i] - D[i] ); expect(that % error < 1e-16) << i + 30;
-    }
-    
-    Benchmark<decltype(&mm_2x2)> bench("mm_2x2 benchmark");
-    data_loader(A, B, C);
+    __m256d const a = _mm256_load_pd( A );
+    __m256d const b = _mm256_load_pd( B );
+ 
+    Benchmark<decltype(&_mm_2x2_avx_v1)> bench("mm_2x2 benchmark");
+
     bench.data_loader = data_loader; // To guarantee same inputs
-    // Add your functions to the struct, give it a name (Should describe improvements there) and yield the flops this function has to do (=work)
-    // First function should always be the base case you want to benchmark against!
-    bench.add_function(&mm_2x2, "base", 0.0);
+    
+    bench.add_function(&mm_2x2_reg, "base", 0.0);
     bench.funcFlops[0] = mm_2x2_flops(A, B, C);
     bench.funcBytes[0] = 8*mm_2x2_memory(A, B, C);
-
-#ifdef __FMA__
-    bench.add_function(&mm_2x2_avx_v1, "avx_v1", 0.0);
+ 
+    bench.add_function(&_mm_2x2_avx_v1, "avx_v1", 0.0);
     bench.funcFlops[1] = mm_2x2_flops(A, B, C);
     bench.funcBytes[1] = 8*mm_2x2_memory(A, B, C);
-    bench.add_function(&mm_2x2_avx_v2, "avx_v2", 0.0);
-    bench.funcFlops[2] = mm_2x2_flops(A, B, C);
-    bench.funcBytes[2] = 8*mm_2x2_memory(A, B, C);
-    bench.add_function(&mm_2x2_avx_v3, "avx_v2", 0.0);
-    bench.funcFlops[3] = mm_2x2_flops(A, B, C);
-    bench.funcBytes[3] = 8*mm_2x2_memory(A, B, C);
-#endif
 
-    bench.run_benchmark(A, B, C);
+    bench.run_benchmark(a, b);
 
     return 0;
 }
